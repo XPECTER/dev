c0de2831ffe64d87c56ffe8e6f6488af73a40885
xpecter <menistream@gmail.com>
2024-03-04 16:45:11 +0900

    infra : apply typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      // synchronize: false, // production 환경에서 true로 사용하지 말것. 데이터를 잃어버림
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
