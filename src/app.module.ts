df9b54d1e2c87d8773f9e74a5718112e7110d165
xpecter <menistream@gmail.com>
2024-03-05 17:36:14 +0900

    feat : add ConfigModule
    - 환경변수 관리를 위한 config 모듈 추가
    - 환경변수 validation을 위한 Joi 라이브러리 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import {
+  validationSchema,
+  validationOptions,
+} from './config/validatation.schema';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // ignoreEnvFile: true // disable env
+      envFilePath: '.env',
+      // isGlobal: true, // 다른 모듈에서 import 하지 않아도 사용할 수 있다.
+      validationSchema,
+      validationOptions,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -15,6 +28,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       // synchronize: false, // production 환경에서 true로 사용하지 말것. 데이터를 잃어버림
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/validatation.schema.ts
@@ -0,0 +1,21 @@
+import * as Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  // env
+  PORT: Joi.number().required(),
+
+  // database
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+});
+
+/**
+ * https://joi.dev/api/?v=17.12.2#anyvalidatevalue-options
+ */
+export const validationOptions = {
+  allowUnkown: true,
+  abortEarly: false,
+};
