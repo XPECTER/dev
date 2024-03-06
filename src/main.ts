d204789437a1896a77cce277ba99845bcbdbe6c0
xpecter <menistream@gmail.com>
2024-03-06 13:02:34 +0900

    feat : use global pipe for validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: configService.get('EXE_ENV') === 'prod',
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
