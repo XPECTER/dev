7a69e5283c704736f9d5cb1e02b792600a8c5489
xpecter <menistream@gmail.com>
2024-03-07 18:44:43 +0900

    refactor : UserService 분리
    - AuthService에 있던 User관련 기능을 UserService로 분리했습니다.
    - 인증, 인가에 관한 내용이 아니기에 분리했습니다.

---
+++ src/auth/user.service.ts
@@ -0,0 +1,38 @@
+import {
+  BadRequestException,
+  UnauthorizedException,
+  Injectable,
+} from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import * as argon from 'argon2';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly userRepo: UserRepository) {}
+
+  async createUser(dto: CreateUserDto): Promise<User> {
+    const user = await this.userRepo.findOneByEmail(dto.email);
+    if (user) {
+      throw new BadRequestException(`'${dto.email}' email already exist`);
+    }
+
+    // argon, scrypt, bcrypt에 대한 내용은 아래 글을 읽어보세요.
+    // https://myas92.medium.com/what-is-the-best-algorithm-bcrypt-scrypt-sha512-argon2-for-password-hashing-in-node-js-2-918b3e49e0b3
+    const hashedPassword = await argon.hash(dto.password);
+    return this.userRepo.createUser(dto, hashedPassword);
+  }
+
+  async verifyUser(email: string, inputPassword: string): Promise<User> {
+    const user = await this.userRepo.findOneByEmail(email);
+
+    if (!user || !(await argon.verify(user.password, inputPassword))) {
+      throw new UnauthorizedException(
+        `Please, check email and password you entered`,
+      );
+    }
+
+    return user;
+  }
+}
